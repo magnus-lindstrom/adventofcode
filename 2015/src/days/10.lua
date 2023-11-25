ed4ba4aface2cf7e00dca12dd97b94c330b01510
Magnus Lindstrom <magnus.lindstrom@tuta.io>
2023-11-25 19:23:05 +0100

    clean up day10, remove comments

--- 2015/src/days/10.lua
+++ 2015/src/days/10.lua
@@ -1,26 +1,5 @@
 local u = require 'src/utils'
 
-local function tbl_from_str(str)
-  local tbl = {}
-  local i = 1
-  local index = 1
-  while i <= str:len() do
-    local digit = str:sub(i,i)
-    local reps = 1
-    for j = i + 1, str:len() do
-      if str:sub(j,j) == digit then
-        reps = reps + 1
-      else
-        break
-      end
-    end
-    tbl[index] = {[reps] = tonumber(digit)}
-    index = index + 1
-    i = i + reps
-  end
-  return tbl
-end
-
 local function tbl_from_str_2(str)
   local tbl = {}
   for i = 1, #str do
@@ -29,47 +8,12 @@ local function tbl_from_str_2(str)
   return tbl
 end
 
-local function evolve_tbl(table)
-  local output = {}
-  local current_digit
-  local reps
-  local i = 0 -- start at 0 because first index must be 1 for ipairs to work later
-
-  for _, inner_table in ipairs(table) do
-    for k, v in pairs(inner_table) do
-      if k == current_digit then
-        reps = reps + 1
-      else
-        if current_digit ~= nil then
-          output[i] = {[reps] = current_digit}
-        end
-        i = i + 1
-        current_digit = k
-        reps = 1
-      end
-
-      if v == current_digit then
-        reps = reps + 1
-      else
-        output[i] = {[reps] = current_digit}
-        i = i + 1
-        current_digit = v
-        reps = 1
-      end
-    end
-  end
-  output[i] = {[reps] = current_digit}
-  return output
-end
-
 local function evolve_tbl_2(tbl)
   local output = {}
   local current_digit = tbl[1]
   local reps = 1
-  -- print('before for loop. cur_dig:', current_digit, 'reps:', reps)
 
   for i = 2, u.table_length(tbl) do
-    -- print(string.format('i: %d, current_digit: %d, tbl[i]: %d', i, current_digit, tbl[i]))
     if current_digit == tbl[i] then
       reps = reps + 1
     else -- a digit streak is broken
@@ -78,7 +22,6 @@ local function evolve_tbl_2(tbl)
       current_digit = tbl[i]
       reps = 1
     end
-    -- print('after for loop. reps:', reps)
 
   end
   -- add last streak since it won't be added in the loop
@@ -86,25 +29,6 @@ local function evolve_tbl_2(tbl)
   output[#output+1] = current_digit
 
   return output
-end
-
-local function str_len_from_tbl(table)
-  local len = 0
-  for _, inner_table in ipairs(table) do
-    for k, v in pairs(inner_table) do
-      local nr_as_string = tostring(v)
-      len = len + k * nr_as_string:len()
-    end
-  end
-  return len
-end
-
-local function str_len_from_tbl_2(tbl)
-  local len = 0
-  for i = 1, u.table_length(tbl), 2 do
-    len = len + tbl[i]
-  end
-  return len
 end
 
 local function main(times)
@@ -142,8 +66,5 @@ end
 local function b()
   return main(50)
 end
-
--- print(a())
--- print(b())
 
 return {a = a, b = b}
