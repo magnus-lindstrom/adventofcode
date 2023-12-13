4db6db0af8fc6c7b2e17167d0ee49d965ff86c91
Magnus Lindstrom <magnus.lindstrom@tuta.io>
2023-12-13 07:42:48 +0100

    cleanup

--- 2023/src/13.py
+++ 2023/src/13.py
@@ -1,129 +1,75 @@
 import pathlib
 
 
-def get_test_input():
-    q_nr = pathlib.Path(__file__).stem
-    file_name = pathlib.Path('inputs/' + q_nr + '_test')
-    with open(file=file_name) as f:
-        return [line.strip() for line in f.readlines()]
-
 def get_input():
     q_nr = pathlib.Path(__file__).stem
     file_name = pathlib.Path('inputs/' + q_nr)
     with open(file=file_name) as f:
         return [line.strip() for line in f.readlines()]
 
-def find_rows_above_reflection_line(field):
+def find_rows_above_reflection_line(field, max_corrections=0):
     for i_line in range(len(field) - 1):
-        if field[i_line] == field[i_line+1]:
-            all_lines_match = True
-            for j in range(1, len(field)):
-                if not (0 <= i_line-j and i_line+j+1 < len(field)):
-                    break
-                if field[i_line-j] != field[i_line+j+1]:
-                    all_lines_match = False
-                    break
-            if all_lines_match:
-                return i_line+1
-    return 0
-
-def find_rows_above_reflection_line_b(field):
-    for i_line in range(len(field) - 1):
-        has_corrected = False
-        # first pair of reflected rows match. One of the other reflections must
-        # have an imperfection
-        if field[i_line] == field[i_line+1]:
-            all_lines_match = True
-            for j in range(1, len(field)):
-                if not (0 <= i_line-j and i_line+j+1 < len(field)):
-                    break
-                if field[i_line-j] != field[i_line+j+1]:
+        corrections_made = 0
+        all_lines_match = True
+        for j in range(0, len(field)):
+            if not (0 <= i_line-j and i_line+j+1 < len(field)):
+                break
+            if field[i_line-j] != field[i_line+j+1]:
+                if corrections_made < max_corrections:
                     unequal_elements = [True for i, j in zip(field[i_line-j], field[i_line+j+1]) if i != j]
-                    if (not has_corrected and len(unequal_elements) == 1):
-                        has_corrected = True
+                    if len(unequal_elements) == 1:
+                        corrections_made += 1
                     else:
                         all_lines_match = False
                         break
-            if has_corrected and all_lines_match:
-                return i_line+1
-
-        # the first reflection has an imperfection, the rest of the reflections
-        # must be perfect
-        elif 1 == len([True for i, j in zip(field[i_line], field[i_line+1]) if i != j]):
-            all_lines_match = True
-            for j in range(1, len(field)):
-                if not (0 <= i_line-j and i_line+j+1 < len(field)):
-                    break
-                if field[i_line-j] != field[i_line+j+1]:
+                else:
                     all_lines_match = False
                     break
-            if all_lines_match:
-                return i_line+1
+        if corrections_made == max_corrections and all_lines_match:
+            return i_line+1
+
     return 0
 
-def a():
+def task(max_corrections):
     inp = get_input()
-    #inp = get_test_input()
     fields = []
-    f = []
+    field = []
     summ = 0
 
     for line in inp:
         if line == '':
-            fields.append(f)
-            f = []
+            fields.append(field)
+            field = []
         else:
-            f.append(line)
-    fields.append(f)
+            field.append(list(line))
+    fields.append(field)
 
     # transpose
     t_fields = []
     for field in fields:
-        t_f = []
-        for i_col in range(len(field[0])):
-            string = ''
-            for i_row in range(len(field)):
-                string += field[i_row][i_col]
-            t_f.append(string)
-        t_fields.append(t_f)
-
-    for ii in range(len(fields)):
-        summ += 100 * find_rows_above_reflection_line(fields[ii])
-        summ += find_rows_above_reflection_line(t_fields[ii])
-
-    return summ
-
-def b():
-    inp = get_input()
-    #inp = get_test_input()
-    fields = []
-    f = []
-    summ = 0
-
-    for line in inp:
-        if line == '':
-            fields.append(f)
-            f = []
-        else:
-            f.append(list(line))
-    fields.append(f)
-
-    # transpose
-    t_fields = []
-    for field in fields:
-        t_f = []
+        t_field = []
         for i_col in range(len(field[0])):
             row = []
             for i_row in range(len(field)):
                 row.append(field[i_row][i_col])
-            t_f.append(row)
-        t_fields.append(t_f)
+            t_field.append(row)
+        t_fields.append(t_field)
 
     for ii in range(len(fields)):
-        summ += 100 * find_rows_above_reflection_line_b(fields[ii])
-        summ += find_rows_above_reflection_line_b(t_fields[ii])
+        summ += 100 * find_rows_above_reflection_line(
+            fields[ii], max_corrections=max_corrections
+        )
+        summ += find_rows_above_reflection_line(
+            t_fields[ii], max_corrections=max_corrections
+        )
 
     return summ
+
+def a():
+    return task(max_corrections=0)
+
+def b():
+    return task(max_corrections=1)
 
 def test_a():
     assert a() == 34889
