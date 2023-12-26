362a5b6537f92088ec394203ff754989a109f216
Magnus Lindstrom <magnus.lindstrom@tuta.io>
2023-12-26 08:39:12 +0100

    2016d6 and start on 2016d7

---
+++ 2016/src/6.py
@@ -0,0 +1,71 @@
+import argparse
+from operator import itemgetter
+import pathlib
+
+
+def get_input(test=False):
+    q_nr = pathlib.Path(__file__).stem
+    if test:
+        file_name = pathlib.Path('inputs/' + q_nr + '_test')
+    else:
+        file_name = pathlib.Path('inputs/' + q_nr)
+    with open(file=file_name) as f:
+        return [line.strip() for line in f.readlines()]
+
+def a(inp):
+    repetitions = {}
+    for line in inp:
+        for i_char, char in enumerate(line):
+            if i_char not in repetitions:
+                repetitions[i_char] = {char: 1}
+            elif char not in repetitions[i_char]:
+                repetitions[i_char][char] = 1
+            else:
+                repetitions[i_char][char] += 1
+    password = ''
+    for key in repetitions.keys():
+        x = [[char, reps] for char, reps in repetitions[key].items()]
+        x.sort(key=itemgetter(1), reverse=True)
+        password += x[0][0]
+
+    return password
+
+def b(inp):
+    repetitions = {}
+    for line in inp:
+        for i_char, char in enumerate(line):
+            if i_char not in repetitions:
+                repetitions[i_char] = {char: 1}
+            elif char not in repetitions[i_char]:
+                repetitions[i_char][char] = 1
+            else:
+                repetitions[i_char][char] += 1
+    password = ''
+    for key in repetitions.keys():
+        x = [[char, reps] for char, reps in repetitions[key].items()]
+        x.sort(key=itemgetter(1))
+        password += x[0][0]
+
+    return password
+
+def test_a():
+    assert a(get_input()) == 0
+
+def test_b():
+    assert b(get_input()) == 0
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--test', dest='test', action='store_true')
+    parser.add_argument('-p', '--profile', dest='profile', action='store_true')
+    args = parser.parse_args()
+    inp = get_input(test=args.test)
+
+    if args.profile:
+        print('\n### Profiling part 1 ###\n')
+        __import__('cProfile').run('a(inp)')
+        print('### Profiling part 2 ###\n')
+        __import__('cProfile').run('b(inp)')
+    else:
+        print('a:', a(inp))
+        print('b:', b(inp))
---
+++ 2016/src/7.py
@@ -0,0 +1,120 @@
+import argparse
+import pathlib
+
+
+def get_input(test=False):
+    q_nr = pathlib.Path(__file__).stem
+    if test:
+        file_name = pathlib.Path('inputs/' + q_nr + '_test')
+    else:
+        file_name = pathlib.Path('inputs/' + q_nr)
+    with open(file=file_name) as f:
+        return [line.strip() for line in f.readlines()]
+
+def has_abba(string):
+    for i_char in range(len(string) - 3):
+        c1 = string[i_char]
+        c2 = string[i_char+1]
+        c3 = string[i_char+2]
+        c4 = string[i_char+3]
+        if c1 == c4 and c2 == c3 and c1 != c2:
+            return True
+    return False
+
+def a(inp):
+
+    supporters = 0
+    for line in inp:
+        supports_tls = False
+        open_brackets = [i for i in range(len(line)) if line[i]  == '[']
+        close_brackets = [i for i in range(len(line)) if line[i]  == ']']
+
+        if has_abba(line[:open_brackets[0]]):
+            supports_tls = True
+        for i in range(len(open_brackets) - 1):
+            if has_abba(line[close_brackets[i]+1:open_brackets[i+1]]):
+                supports_tls = True
+
+        if has_abba(line[close_brackets[-1]+1:]):
+            supports_tls = True
+
+        for i in range(len(open_brackets)):
+            if has_abba(line[open_brackets[i]+1:close_brackets[i]]):
+                supports_tls = False
+
+        if supports_tls:
+            supporters += 1
+
+    return supporters
+
+def get_abas(string):
+    abas = []
+    for i_char in range(len(string) - 2):
+        c1 = string[i_char]
+        c2 = string[i_char+1]
+        c3 = string[i_char+2]
+        if c1 == c3 and c1 != c2:
+            abas.append(c1+c2+c3)
+    return abas
+
+def has_bab(string, aba):
+    bab = aba[1] + aba[0] + aba[1]
+    for i_char in range(len(string) - 2):
+        s = string[i_char:i_char + 3]
+        if s == bab:
+            return True
+    return False
+
+def b(inp):
+    supporters = 0
+    for line in inp:
+        line = 'aaa[jaj]baja'
+        supports_ssl = False
+        open_brackets = [i for i in range(len(line)) if line[i]  == '[']
+        close_brackets = [i for i in range(len(line)) if line[i]  == ']']
+
+        abas = get_abas(line[:open_brackets[0]])
+        for i in range(len(open_brackets) - 1):
+            for aba in get_abas(line[close_brackets[i]+1:open_brackets[i+1]]):
+                abas.append(aba)
+        for aba in get_abas(line[close_brackets[-1]+1:]):
+            abas.append(aba)
+        abas = set(abas)
+        print(line)
+        #print(abas)
+        #print()
+
+        for aba in abas:
+            supports_tls = False
+            for i in range(len(open_brackets)):
+                if has_bab(line[open_brackets[i]+1:close_brackets[i]], aba):
+                    supports_ssl = True
+
+        if supports_ssl:
+            print('true')
+            supporters += 1
+        return
+
+    return supporters
+
+def test_a():
+    assert a(get_input()) == 0
+
+def test_b():
+    assert b(get_input()) == 0
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--test', dest='test', action='store_true')
+    parser.add_argument('-p', '--profile', dest='profile', action='store_true')
+    args = parser.parse_args()
+    inp = get_input(test=args.test)
+
+    if args.profile:
+        print('\n### Profiling part 1 ###\n')
+        __import__('cProfile').run('a(inp)')
+        print('### Profiling part 2 ###\n')
+        __import__('cProfile').run('b(inp)')
+    else:
+        print('a:', a(inp))
+        print('b:', b(inp))
