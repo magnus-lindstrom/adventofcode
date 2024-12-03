e0e2c0e56a10dcd7b943c400fa2b9d8ebac7751f
Magnus Lindstrom <magnus.lindstrom@mullvad.net>
2024-12-03 10:08:59 +0100

    add 2024d3

--- 2024/src/3.py
+++ 2024/src/3.py
@@ -17,7 +17,6 @@ def get_mult_sum(line):
     p = re.findall(r"mul\((\d+),(\d+)\)", line)
     for m in p:
         summ += (int(m[0]) * int(m[1]))
-    #print(p.groups())
     return summ
 
 def a(inp):
@@ -28,62 +27,50 @@ def a(inp):
 
 def b(inp):
     summ = 0
-    for line in inp:
-        print()
-        do_spans = []
-        dont_spans = []
-        for m in re.finditer(r"do\(\)", line):
-            do_spans.append(m.span())
-        print('dos:', do_spans)
-        for m in re.finditer(r"don't\(\)", line):
-            dont_spans.append(m.span())
-        print('don\'ts:', dont_spans)
-        print(f'line length: {len(line)}')
+    line = ''.join(inp)
 
-        i_do = 0
-        # i_dont is the first character after the text "don't"
-        if dont_spans:
-            i_dont = dont_spans[0][1]
-        else:
-            i_dont = 10000000
+    dos = []
+    donts = []
+    for m in re.finditer(r"do\(\)", line):
+        dos.append(m.span()[1])
+    for m in re.finditer(r"don't\(\)", line):
+        donts.append(m.span()[1])
 
-        more_substrings_to_check = True
-        while(more_substrings_to_check):
-            more_substrings_to_check = False
-            print(f'checking {i_do} - {i_dont}')
+    # i_do is the first character after the text "do()"
+    i_do = 0
+    # i_dont is the first character after the text "don't()"
+    i_dont = donts[0]
 
-            if i_dont > len(line):
-                extra = get_mult_sum(line[i_do:])
-                summ += extra
-                print('adding ', extra)
-            else:
-                extra = get_mult_sum(line[i_do:i_dont])
-                summ += extra
-                print('Adding ', extra)
+    more_substrings_to_check = True
+    while(more_substrings_to_check):
+        more_substrings_to_check = False
 
-            # update i_do
-            for span in do_spans:
-                if span[1] > i_dont:
-                    i_do = span[1]
-                    more_substrings_to_check = True
-                    break
+        extra = get_mult_sum(line[i_do:i_dont])
+        summ += extra
 
-            # update i_dont
-            for span in dont_spans:
-                if span[1] > i_do:
-                    i_dont = span[1]
-                    break
+        # update i_do
+        for start in dos:
+            if start > i_dont:
+                i_do = start
+                more_substrings_to_check = True
+                break
 
-            if i_dont < i_do:
-                i_dont = 10000000
+        # update i_dont
+        for start in donts:
+            if start > i_do:
+                i_dont = start
+                break
+
+        if i_dont < i_do:
+            i_dont = len(line) - 1
 
     return summ
 
 def test_a():
-    assert a(get_input()) == 0
+    assert a(get_input()) == 166630675
 
 def test_b():
-    assert b(get_input()) == 0
+    assert b(get_input()) == 93465710
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
